a49bc89106f143c179ceec96f0d380f862dfe553
Bianca Minnaar <bminnaar@gmail.com>
2018-02-27 13:35:40 +0200

    Normalise source base

---
+++ Xamarin/ResPublica/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace ResPublica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //var _MasterRepo = MasterRepository.MasterRepo;
+            //_MasterRepo.SetRootView(new NavigationPage(new LoginView()));
+            ////_MasterRepo.SetRootView(new NavigationPage(new TestHarnesView()));
+            //MainPage = _MasterRepo.GetRootView();
+        }
+    }
+}
---
+++ Xamarin/ResPublica/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ResPublica.PCL
+{
+    public static class Constants
+    {
+        public static string APPLICATION_TYPE = "application/json";
+        // URL of REST service
+        public static string BASE_URL = "http://localhost:8080/api";
+        public static int SCREEN_AVAILABLE_HEIGHT = 100;
+    }
+}
