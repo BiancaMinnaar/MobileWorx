93310e8727c4d8021365a489478794f7987f1236
Bianca Minnaar <bminnaar@gmail.com>
2018-02-28 11:50:49 +0200

    Add Register button to Login view - Add XAML for StackLayout - Add XAML for Button - Add XAML binding Attribute for command - Add OnRegister ICommand to LoginViewModel

--- Xamarin/ResPublica/ViewModel/LoginViewModel.cs
+++ Xamarin/ResPublica/ViewModel/LoginViewModel.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Windows.Input;
 using ResPublica.Base;
 
 namespace ResPublica.Implementation.ViewModel
 {
     public class LoginViewModel : ProjectBaseViewModel
     {
-        
+        public ICommand OnRegister { get; set; }
     }
 }
