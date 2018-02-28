c732cce10b3cdc60bed95caf8ff7d589cb28ceff
Bianca Minnaar <bminnaar@gmail.com>
2018-02-28 17:31:10 +0200

    Show login image

--- Xamarin/ResPublica/ViewModel/LoginViewModel.cs
+++ Xamarin/ResPublica/ViewModel/LoginViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using System.Windows.Input;
 using ResPublica.Base;
 
@@ -6,6 +8,21 @@ namespace ResPublica.Implementation.ViewModel
 {
     public class LoginViewModel : ProjectBaseViewModel
     {
-        public ICommand OnRegister { get; set; }
+        public byte[] BackGround
+        {
+            get
+            {
+                var assembly = typeof(App).GetTypeInfo().Assembly;
+                byte[] buffer;
+                using (Stream s = assembly.GetManifestResourceStream("ResPublica.Images.Login_Background.jpg"))
+                {
+                    long length = s.Length;
+                    buffer = new byte[length];
+                    s.Read(buffer, 0, (int)length);
+
+                    return buffer;
+                }
+            }
+        }
     }
 }
