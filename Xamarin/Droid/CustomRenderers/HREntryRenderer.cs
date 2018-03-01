b716b4236bdd69f6ad8e76a3d8d16e4f41b52134
= <johan.jvr1@gmail.com>
2018-03-01 02:46:19 +0200

    implemented the login screen

--- Xamarin/Droid/CustomRenderers/HREntryRenderer.cs
+++ Xamarin/Droid/CustomRenderers/HREntryRenderer.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.V4.Content;
@@ -20,26 +22,54 @@ namespace ResPublica.Droid.CustomRenderers
 {
 	public class HREntryRenderer : EntryRenderer
 	{
+		HREntry element;
 		public HREntryRenderer(Context context) : base(context)
 		{
-		}
-
-		public HREntryRenderer()
-		{
-
 		}
 
 		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
 		{
 			base.OnElementChanged(e);
 
+			if (e.OldElement != null || e.NewElement == null)
+				return;
+
+			element = (HREntry)this.Element;
+
+			var editText = this.Control;
+			if (!string.IsNullOrEmpty(element.Image))
+			{
+				switch (element.ImageAlignment)
+				{
+					case ImageAlignment.Left:
+						editText.SetCompoundDrawablesWithIntrinsicBounds(GetDrawable(element.Image), null, null, null);
+						break;
+					case ImageAlignment.Right:
+						editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, GetDrawable(element.Image), null);
+						break;
+				}
+			}
+			editText.CompoundDrawablePadding = 25;
+
+
 			if (Control != null)
 			{
 				var background = ContextCompat.GetDrawable(Context, Resource.Drawable.RoundedCornerEntry);
 
 				Control.Background = background;
-				Control.SetPadding(10, 10, 10, 3);
+				Control.SetPadding(10, 20, 10, 20);
+				
 			}
+		}
+
+		private BitmapDrawable GetDrawable(string imageEntryImage)
+		{
+			var bitmap = Resources.GetBitmap(imageEntryImage);
+			//int resID = Resources.GetIdentifier(imageEntryImage, "drawable", this.Context.PackageName);
+			//var drawable = ContextCompat.GetDrawable(this.Context, resID);
+			//var bitmap = ((BitmapDrawable)drawable).Bitmap;
+
+			return new BitmapDrawable(Resources, Bitmap.CreateScaledBitmap(bitmap, element.ImageWidth * 2, element.ImageHeight * 2, true));
 		}
 	}
 
--- Xamarin/ResPublica/View/Controls/HREntry.cs
+++ Xamarin/ResPublica/View/Controls/HREntry.cs
@@ -9,5 +9,42 @@ namespace ResPublica.View.Controls
 {
 	public class HREntry : Entry
 	{
+		public static readonly BindableProperty ImageProperty = BindableProperty.Create(nameof(Image), typeof(string), typeof(HREntry), string.Empty);
+
+		public static readonly BindableProperty ImageHeightProperty =	 BindableProperty.Create(nameof(ImageHeight), typeof(int), typeof(HREntry), 40);
+
+		public static readonly BindableProperty ImageWidthProperty = BindableProperty.Create(nameof(ImageWidth), typeof(int), typeof(HREntry), 40);
+
+		public static readonly BindableProperty ImageAlignmentProperty = BindableProperty.Create(nameof(ImageAlignment), typeof(ImageAlignment), typeof(HREntry), ImageAlignment.Left);
+
+		public int ImageWidth
+		{
+			get { return (int)GetValue(ImageWidthProperty); }
+			set { SetValue(ImageWidthProperty, value); }
+		}
+
+		public int ImageHeight
+		{
+			get { return (int)GetValue(ImageHeightProperty); }
+			set { SetValue(ImageHeightProperty, value); }
+		}
+
+		public string Image
+		{
+			get { return (string)GetValue(ImageProperty); }
+			set { SetValue(ImageProperty, value); }
+		}
+
+		public ImageAlignment ImageAlignment
+		{
+			get { return (ImageAlignment)GetValue(ImageAlignmentProperty); }
+			set { SetValue(ImageAlignmentProperty, value); }
+		}
+	}
+
+	public enum ImageAlignment
+	{
+		Left,
+		Right
 	}
 }
