9a74bd7b1ee84b54251ed220295a7f9d87386086
Bianca Minnaar <bminnaar@gmail.com>
2018-03-05 08:06:38 +0200

    Add UserModel - Add UserModel - Add UserModel reference to MasterModel

--- Xamarin/ResPublica/ViewModel/MasterModel.cs
+++ Xamarin/ResPublica/ViewModel/MasterModel.cs
@@ -3,5 +3,6 @@
     public class MasterModel
     {
         public bool Authenticated { get; set; }
+        public UserModel User { get; set; }
     }
 }
---
+++ Xamarin/ResPublica/ViewModel/UserModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace HiRes.ViewModel
+{
+    public class UserModel
+    {
+        
+    }
+}
