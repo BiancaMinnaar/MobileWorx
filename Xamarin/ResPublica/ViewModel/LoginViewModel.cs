9c57b6ede4c53b50a1222358d5245b0a3f896cd2
= <johan.jvr1@gmail.com>
2018-03-07 04:21:28 +0200

    updated the UI

--- Xamarin/ResPublica/ViewModel/LoginViewModel.cs
+++ Xamarin/ResPublica/ViewModel/LoginViewModel.cs
@@ -29,7 +29,7 @@ namespace HiRes.Implementation.ViewModel
 
         public string SvgFileName{ get { return "CallStartTime.svg"; }}
 
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
 		[JsonProperty(PropertyName ="X-API-TOKEN")]
 		public string XAPITOKEN { get; set; } = "boguskey_rsl";
