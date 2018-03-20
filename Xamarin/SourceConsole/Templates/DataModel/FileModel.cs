a57fac93389c2568cfcda36c8b1641991661261c
Bianca Minnaar <bminnaar@gmail.com>
2018-03-20 15:36:07 +0200

    Configure correct classPath

--- Xamarin/SourceConsole/Templates/DataModel/FileModel.cs
+++ Xamarin/SourceConsole/Templates/DataModel/FileModel.cs
@@ -6,6 +6,13 @@
         public string Extension { get; set; }
         public string FileName { get { return CodeName + '.' + Extension; }}
         public string ProjectFilePath { get; set; }
-        public string FullProjectFileName { get { return ProjectFilePath + "//" + FileName; }}
+        public string FullProjectFileName 
+        { 
+            get 
+            { 
+                var cleanPath = ProjectFilePath.Replace('/', '\\').Split(new string[] {"..\\"}, System.StringSplitOptions.None);
+                return cleanPath[cleanPath.Length-1] + FileName; 
+            }
+        }
     }
 }
