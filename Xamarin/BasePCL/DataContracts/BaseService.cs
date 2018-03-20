3ff68a57ea6009b741722a5d867d1124b62cea63
Bianca Minnaar <bminnaar@gmail.com>
2018-03-20 21:28:21 +0200

    Move BaseService to BasePCL

--- Xamarin/BasePCL/DataContracts/BaseService.cs
+++ Xamarin/BasePCL/DataContracts/BaseService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using CorePCL;
 
-namespace HiRes.Base
+namespace CorePCL
 {
     public class BaseService<T>
         where T:BaseViewModel
